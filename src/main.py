00c399b958c07beb9fe033059d3fead12e3af54a
Jian Hang <james.jian.hang@gmail.com>
2025-03-03 06:23:34 +1100

    initial

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def read_index():
+	"""
+	Return a  Python Dictionary that supports JSON serialization.
+	"""
+	return {"Hello":"World"}
+
+
+@app.get("/api/v1/hello-world")
+def read_index_v1():
+	"""
+	Return a  Python Dictionary that supports JSON serialization.
+	"""
+	return {"Hello":"World", "version": "v1"}
+
+
